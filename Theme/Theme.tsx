4e61c5741bdd84e708ce00596cce8b799f26a5dc
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-10-04 00:31:42 -0300

    Style: Mudança de tema

--- Theme/Theme.tsx
+++ Theme/Theme.tsx
@@ -1,11 +1,32 @@
-export const theme = {
+// export const theme = {
+//     color: {
+//         primary: '#EEC643',
+//         yellow: '#F7CE00',
+//         positive: '#B5F500',
+//         negative: '#EF8354',
+//         gray: '#333333',
+//         background: "#141414",
+//         background1: "#f5f5f5",
+//         background2: "#273978",
+//         statusBar: "#171f4a",
+//         secondary: "#707EFF",
+//         headerMenu: "rgba(67, 80, 192, 0.1);",
+//       },
+//     fontFamily: {
+//       Regular: 'SourceSans3-Medium',
+//       Thin: 'SourceSans3-Light',
+//       Bold: 'SourceSans3-Bold',
+//     },
+//   };
+  
+  export const theme = {
     color: {
-        primary: '#EEC643',
+        primary: '#141414',
         yellow: '#F7CE00',
         positive: '#B5F500',
         negative: '#EF8354',
         gray: '#333333',
-        background: "#141414",
+        background: "#fffff0",
         background1: "#f5f5f5",
         background2: "#273978",
         statusBar: "#171f4a",
