8656c94c050004fde31b4efbc6c3a50ebaa84274
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-10-03 21:15:41 -0300

    Style: Alterações visuais

--- Screens/Main.tsx
+++ Screens/Main.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native'; 
-import { theme } from '../Theme/Theme';
+import { ActivityIndicator, Text, View } from 'react-native'; 
 import { Button } from 'tamagui';
 import { CalendarRange, ScanFace, ArrowUpCircle, ArrowDownCircle } from '@tamagui/lucide-icons';
+import { useState } from 'react';
+import { theme } from '../Theme/Theme';
 
 export function Main() {
-
+  const [loading, setLoading] = useState(false);
   const userData = {
     name: "João",
     lastName: "Pedro",
@@ -20,6 +21,14 @@ export function Main() {
     currency: 'BRL',
   }).format(userData.currentBalance);
 
+
+  if (loading)
+		return (
+			<View style={{ backgroundColor: theme.color.background, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<ActivityIndicator size={'large'} color={theme.color.primary} />
+			</View>
+		);
+
   return (
     <View style={{
         flex: 1,
@@ -30,34 +39,32 @@ export function Main() {
         <View
           style={{justifyContent: 'space-between', flexDirection: 'row', width: '85%', marginTop: 32}}
         >
-        <Button bg="$gray2Dark" width="$5" size="$6" icon={<ScanFace color="$yellow10Light" size="$4"/>}/>
-        <Button bg="$gray2Dark" width="$5" size="$6" icon={<CalendarRange color="$yellow10Light" size="$4"/>}/>
+        <Button pressStyle={{backgroundColor: '$gray1Dark'}} bg="$gray3Dark" width="$5" size="$6" icon={<ScanFace color="$yellow10Light" size="$4"/>}/>
+        <Button pressStyle={{backgroundColor: '$gray1Dark'}} bg="$gray3Dark" width="$5" size="$6" icon={<CalendarRange color="$yellow10Light" size="$4"/>}/>
         </View>
         <View style={{
           height: '70%',
           justifyContent: 'center',
         }}>
-          <Text style={{ color: theme.color.yellow ,fontFamily: theme.fontFamily.Regular, fontSize: 56}}>
+          <Text style={{ color: theme.color.primary ,fontFamily: theme.fontFamily.Thin, fontSize: 56}}>
               {formattedBalance}
           </Text>
         </View>
         <View
           style={{justifyContent: 'space-between', flexDirection: 'row', width: '90%', marginTop: 32}}
         >
-        <Button bg="$gray2Dark" size="$7" borderRadius="$10">
+        <Button pressStyle={{backgroundColor: '$gray1Dark'}} bg="$gray3Dark" h="$8" w="$13" borderRadius="$10" iconAfter={<ArrowDownCircle color="$orange10Light" size="$4"/>}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={{ fontFamily: theme.fontFamily.Regular, color: 'white' }}>
+            <Text style={{ fontSize: 16, fontFamily: theme.fontFamily.Regular, color: 'white' }}>
               Despesa
             </Text>
-            <ArrowDownCircle color="$orange10Light" size="$4"/>
           </View>
         </Button>
-        <Button bg="$gray2Dark" size="$7" borderRadius="$10">
+        <Button pressStyle={{backgroundColor: '$gray1Dark'}} bg="$gray3Dark" h="$8" w="$13" borderRadius="$10" iconAfter={<ArrowUpCircle color="$green10Dark" size="$4"/>}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={{ fontFamily: theme.fontFamily.Regular, color: 'white' }}>
+            <Text style={{ fontSize: 16, fontFamily: theme.fontFamily.Regular, color: 'white' }}>
               Receita
             </Text>
-            <ArrowUpCircle color="$green10Dark" size="$4"/>
           </View>
         </Button>
         </View>
